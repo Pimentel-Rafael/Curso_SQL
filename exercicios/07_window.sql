ff74f3802cf6a8bb1da48b18e6c6893a336dcd85
Rafael1799 <rafaeloliveirapimentel@gmail.com>
2026-02-27 17:30:48 -0300

    Atualização

--- exercicios/07_window.sql
+++ exercicios/07_window.sql
@@ -4,15 +4,34 @@ WITH
 
 tb_cliente_semana AS (
 
-SELECT  idCliente,
-        strftime('%w', substr(DtCriacao,1,10)) AS dtDiaSemana,
-        count(DISTINCT IdTransacao) AS qttransacao
+    SELECT  idCliente,
+            strftime('%w', substr(DtCriacao,1,10)) AS dtDiaSemana,
+            count(DISTINCT IdTransacao) AS qttransacao
 
-FROM transacoes
+    FROM transacoes
 
-GROUP BY idCliente, dtDiaSemana
+    GROUP BY idCliente, dtDiaSemana
+),
+
+tb_rn AS (
+
+    SELECT  *,
+            CASE
+                WHEN dtDiaSemana = '1' THEN 'SEGUNDA-FEIRA'
+                WHEN dtDiaSemana = '2' THEN 'TERÇA-FEIRA'
+                WHEN dtDiaSemana = '3' THEN 'QUARTA-FEIRA'
+                WHEN dtDiaSemana = '4' THEN 'QUINTA-FEIRA'
+                WHEN dtDiaSemana = '5' THEN 'SEXTA-FEIRA'
+                WHEN dtDiaSemana = '6' THEN 'SABADO'
+                ELSE 'DOMINGO'
+                END AS DescDiaSemana,
+
+            ROW_NUMBER() OVER (PARTITION BY idCliente ORDER BY qttransacao DESC) AS rn
+
+    FROM tb_cliente_semana
+
 )
 
-SELECT *
-
-FROM tb_cliente_semana
+SELECT * 
+FROM tb_rn
+WHERE rn = 1

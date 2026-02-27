16987b6b655dcb5f30c4e737a1c2e0e4596e0eb3
Rafael1799 <rafaeloliveirapimentel@gmail.com>
2026-02-27 17:19:05 -0300

    Atualização

--- exercicios/07_window.sql
+++ exercicios/07_window.sql
@@ -5,6 +5,14 @@ WITH
 tb_cliente_semana AS (
 
 SELECT  idCliente,
-        strftime('%w'substr(DtCriacao,1,10))
+        strftime('%w', substr(DtCriacao,1,10)) AS dtDiaSemana,
+        count(DISTINCT IdTransacao) AS qttransacao
 
+FROM transacoes
+
+GROUP BY idCliente, dtDiaSemana
 )
+
+SELECT *
+
+FROM tb_cliente_semana
